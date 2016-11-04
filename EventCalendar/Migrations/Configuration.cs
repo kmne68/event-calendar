4a7805b90544d7c7b5a70e8adbb1429074f7cdf7
Joe Arcelo <josepharcelo@gmail.com>
2016-11-04 11:41:51 -0500

    Add seed data in Configurations.cs

--- EventCalendar/Migrations/Configuration.cs
+++ EventCalendar/Migrations/Configuration.cs
@@ -2,45 +2,36 @@ namespace EventCalendar.Migrations
 {
     using Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<EventCalendar.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(EventCalendar.Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var events = new List<Events>
+            {
+                new Events { EventTitle = "Keith's Bday",
+                    Date = DateTime.ParseExact("11/10/2016", "0:dd/MM/yyyy", null),
+                    StartTime = DateTime.ParseExact("1:30:00", "0:HH:mm", null),
+                    EndTime = DateTime.ParseExact("2:30:00", "0:HH:mm", null) },
+                new Events { EventTitle = "Joe's Graduation",
+                    Date = DateTime.ParseExact("11/12/2016", "0:dd/MM/yyyy", null),
+                    StartTime = DateTime.ParseExact("2:30:00", "0:HH:mm", null),
+                    EndTime = DateTime.ParseExact("3:30:00", "0:HH:mm", null)
+                }
+            };
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-            context.Events.AddOrUpdate(
-                    new Events {
-                        EventTitle = "Event 1",
-                        Date = DateTime.ParseExact("10/10/2016", "0:dd/MM/yyyy", null),
-                        StartTime = DateTime.ParseExact("1:30:00", "0:HH:mm", null),
-                        EndTime = DateTime.ParseExact("2:30:00", "0:HH:mm", null) },
-
-                     new Events {
-                        EventTitle = "Event 2",
-                        Date = DateTime.ParseExact("10/12/2016", "0:dd/MM/yyyy", null),
-                        StartTime = DateTime.ParseExact("2:30:00", "0:HH:mm", null),
-                        EndTime = DateTime.ParseExact("3:30:00", "0:HH:mm", null)
-                     }
-                );
+            // Assumes k.EventTitles are unique | TODO Replace this before production
+            events.ForEach(e => context.Events.AddOrUpdate(k => k.EventTitle, e));
+            context.SaveChanges();
         }
     }
 }
