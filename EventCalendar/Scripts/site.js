69ec4cd89e46950ff6ec617c233ac0b239894d47
Joe Arcelo <josepharcelo@gmail.com>
2016-11-11 20:52:26 -0600

    Add datepicker properties

--- EventCalendar/Scripts/site.js
+++ EventCalendar/Scripts/site.js
@@ -4,26 +4,55 @@
         this.type="text";
     });
 
+    $('input[type=time]').each(function () {
+        this.type = "text";
+    });
+
     $(".date-picker").datepicker({
     //    showOn: "button",
     //    buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
     //    buttonImageOnly: true,
 
-    showOn: "both",
-    altFormat: "mm/dd/yyyy",
-    buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
-    buttonImageOnly: true,
-    buttonText: "Select date",
-    minDate : 0,
-    showButtonPanel: true,
-    closeText: 'Clear',
-    onClose: function () {
-        var event = arguments.callee.caller.caller.arguments[0];
-        // If "Clear" gets clicked, then really clear it
-        if ($(event.delegateTarget).hasClass('ui-datepicker-close')) {
-            $(this).val('');
+        showOn: "both",
+        altFormat: "mm/dd/yyyy",
+        //dateFormat: "mm/dd/yyyy",
+        buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
+        buttonImageOnly: true,
+        buttonText: "Select date",
+        minDate: 0,
+        defaultDate: new Date(),
+        duration: "slow",
+        showButtonPanel: true,
+        closeText: 'Clear',
+        onClose: function () {
+            var event = arguments.callee.caller.caller.arguments[0];
+            // If "Clear" gets clicked, then really clear it
+            //if ($(event.delegateTarget).hasClass('ui-datepicker-close')) {
+            //    $(this).val('');
+            //}
         }
-    }
+    });
+
+    $('.start-time-picker').timepicker({
+        timeFormat: 'h:mm p',
+        interval: 60,
+        minTime: '10',
+        maxTime: '11:00pm',
+        defaultTime: '10',
+        startTime: '6:00',
+        dynamic: false,
+        dropdown: true,
+        scrollbar: true
+    });
+
+    $('.end-time-picker').timepicker({
+        timeFormat: 'h:mm p',
+        interval: 60,
+        minTime: '1:00am',
+        maxTime: '11:00pm',
+        defaultTime: '1:00pm',
+        startTime: '6:00am',
+   
     });
 
 });
