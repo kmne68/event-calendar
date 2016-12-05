624c083ce4d85939754ae0b271789298de2ff705
kmne68 <kmne68@gmail.com>
2016-12-05 13:42:42 -0600

    Add bootswatch and implement Spacelab theme

--- EventCalendar/App_Start/BundleConfig.cs
+++ EventCalendar/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace EventCalendar
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootswatch/spacelab/bootstrap.css",
+                      //"~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
             // Calendar css file
